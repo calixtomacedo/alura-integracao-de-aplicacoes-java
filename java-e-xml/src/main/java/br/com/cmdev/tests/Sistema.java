bccecb5b820bb3d5dae9bd479cc2eee1d8e8df90
Calixto Macedo <calixto.macedo@gmail.com>
2021-08-10 19:00:39 -0300

    <?xml version=1.0 encoding=UTF-8?>

--- java-e-xml/src/main/java/br/com/cmdev/tests/Sistema.java
+++ java-e-xml/src/main/java/br/com/cmdev/tests/Sistema.java
@@ -1,15 +1,11 @@
 package br.com.cmdev.tests;
 
-import java.io.IOException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import br.com.cmdev.model.Produto;
 
@@ -17,21 +13,24 @@ public class Sistema {
 
 	public static void main(String[] args) throws Exception {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setValidating(true);
+		factory.setNamespaceAware(true);
+		factory.setAttribute("http://java.sun.com/xml/jaxp/properties/schemaLanguage", "http://www.w3.org/2001/XMLSchema");
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		Document document = builder.parse("src/vendas.xml");
 		
-		getNome(document);
+		//getNome(document);
 		
 		percorreXML(document);
 	}
-
+/*
 	private static void getNome(Document document) throws ParserConfigurationException, SAXException, IOException {
 		NodeList nome = document.getElementsByTagName("nome");
 		Element elementNome = (Element) nome.item(0);
 		String nomeProduto = elementNome.getTextContent();
 		System.out.println(nomeProduto);
 	}
-
+*/
 	private static void percorreXML(Document document) {
 		NodeList nodeList = document.getElementsByTagName("produto");
 		Element venda = document.getDocumentElement();
