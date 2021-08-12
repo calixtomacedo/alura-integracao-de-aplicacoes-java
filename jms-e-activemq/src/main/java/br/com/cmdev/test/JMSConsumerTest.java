2a565479652b6654477befadbb58538214637cb2
Calixto Macedo <calixto.macedo@gmail.com>
2021-08-12 06:02:21 -0300

    02.Consumindo mensagens com JMS

---
+++ jms-e-activemq/src/main/java/br/com/cmdev/test/JMSConsumerTest.java
@@ -0,0 +1,39 @@
+package br.com.cmdev.test;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+
+public class JMSConsumerTest {
+
+	public static void main(String[] args) throws Exception {
+
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+
+		Connection connection = factory.createConnection();
+		connection.start();
+
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		Destination queue = (Destination) context.lookup("financeiro");
+		MessageConsumer consumer = session.createConsumer(queue);
+
+		Message message = consumer.receive();
+
+		System.out.println("Recebendo nossa mensagem: " + message);
+
+		new Scanner(System.in).nextLine();
+
+		session.close();
+		connection.close();
+		context.close();
+	}
+
+}
