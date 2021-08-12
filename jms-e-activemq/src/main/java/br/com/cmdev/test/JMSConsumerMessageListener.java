3cc6f4421d357dc6e72cf4fade76c7f249e716d8
Calixto Macedo <calixto.macedo@gmail.com>
2021-08-12 06:32:18 -0300

    03.Recebendo mensagens com MessageListener

--- jms-e-activemq/src/main/java/br/com/cmdev/test/JMSConsumerMessageListener.java
+++ jms-e-activemq/src/main/java/br/com/cmdev/test/JMSConsumerMessageListener.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
-public class JMSConsumerTest {
+public class JMSConsumerMessageListener {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) throws Exception {
 
 		InitialContext context = new InitialContext();
@@ -25,9 +29,17 @@ public class JMSConsumerTest {
 		Destination queue = (Destination) context.lookup("financeiro");
 		MessageConsumer consumer = session.createConsumer(queue);
 
-		Message message = consumer.receive();
-
-		System.out.println("Recebendo nossa mensagem: " + message);
+		consumer.setMessageListener(new MessageListener() {
+			@Override
+			public void onMessage(Message message) {
+				TextMessage textMessage = (TextMessage) message;
+				try {
+					System.out.println("Recebendo nossa mensagem: " + textMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 
 		new Scanner(System.in).nextLine();
 
